72ca7f325393a41ed1839da375c470969632dc19
angelz07 <fabrice@telecom4all.be>
2023-01-10 00:17:03 +0100

    Update mysql_dashboard.py

--- bitget_bot_futures/utilities/mysql_dashboard.py
+++ bitget_bot_futures/utilities/mysql_dashboard.py
@@ -14,7 +14,7 @@ class MysqlDashboard():
             print("Erreur de connexion à la base de données: {}".format(err))
             self._auth = False
         else:
-            mycursor = mydb.cursor()
+            mycursor = mydb.cursor(prepared=True)
             self._auth = True
             self.mydb = mydb
             self.mycursor = mycursor
@@ -23,38 +23,38 @@ class MysqlDashboard():
         if self._auth:
             now_recap = datetime.datetime.now()
             date_recap = now_recap.strftime("%Y-%m-%d")
-            self.mycursor.execute("INSERT INTO boll_strat  (date, wallet) VALUES (%s, %s)", (date_recap, usdAmount))
+            sql_query = "INSERT INTO boll_strat  (date, wallet) VALUES (%s, %s)"
+            self.mycursor.execute(sql_query, (date_recap, usdAmount))
             self.mydb.commit()
-            
-    
+
     def log_close_long(self, close_long_quantity, coin, close_long_market_price):
         if self._auth:
             sql = "INSERT INTO boll_strat_orderBook (type, amount, symbol, price) VALUES (%s, %s, %s, %s)"
             val = ("3", close_long_quantity, coin, close_long_market_price)
             self.mycursor.execute(sql, val)
             self.mydb.commit()
-            
+
     def log_open_long(self, long_quantity_in_usd, coin, long_market_price):
         if self._auth:
             sql = "INSERT INTO boll_strat_orderBook (type, amount, symbol, price) VALUES (%s, %s, %s, %s)"
             val = ("2", long_quantity_in_usd, coin, long_market_price)
             self.mycursor.execute(sql, val)
             self.mydb.commit()
-            
+
     def log_close_short(self, exchange_close_short_quantity, coin, close_short_market_price):
         if self._auth:
             sql = "INSERT INTO boll_strat_orderBook (type, amount, symbol, price) VALUES (%s, %s, %s, %s)"
             val = ("4", exchange_close_short_quantity, coin, close_short_market_price)
             self.mycursor.execute(sql, val)
             self.mydb.commit()
-    
+
     def log_open_short(self, short_quantity_in_usd, coin, short_market_price):
         if self._auth:
             sql = "INSERT INTO boll_strat_orderBook (type, amount, symbol, price) VALUES (%s, %s, %s, %s)"
             val = ("1", short_quantity_in_usd, coin, short_market_price)
             self.mycursor.execute(sql, val)
             self.mydb.commit()
-            
+
     def __del__(self):
         if self._auth:
             self.mydb.close()
